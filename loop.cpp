665b2a7dc27089764160b56682e9aafac8086731
kaushiki Mishra <153539596+kaushiki2024@users.noreply.github.com>
2024-03-31 19:38:02 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,10 @@
+//highest factor of a number(other than the number itself)
+#include<iostream>
+int main(){
+    int n;
+    std::cin>>n;
+    int a=1;
+    for(int i=1;i<=n;i=i++){
+        std::cout<<i;
+    }
+}
