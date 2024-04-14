7521b1f9eb82fde819f4a0328f6866c4b2f050e2
kaushiki Mishra <153539596+kaushiki2024@users.noreply.github.com>
2024-04-14 11:29:37 +0530

    Add files via upload

---
+++ q14rombus.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(){
+    int i,j,m;
+    std::cin>>m;
+    for(i=1;i<=m;i++){//rows
+       for(j=1;j<=m-i;j++){//spaces
+           std::cout<<"  ";
+       }
+       for(j=1;j<=m;j++){
+        std::cout<<"* ";
+       }
+       std::cout<<"\n";
+
+    }
+}
+
