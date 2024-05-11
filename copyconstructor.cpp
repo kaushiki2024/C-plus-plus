2ede7bcf10c4b8514060d53452670b05b07fe97b
kaushiki Mishra <153539596+kaushiki2024@users.noreply.github.com>
2024-05-11 12:37:14 +0530

    Add files via upload

--- copyconstructor.cpp
+++ copyconstructor.cpp
@@ -19,7 +19,17 @@ class Customer{
         std::cout<<balance<<"\n";
         std::cout<<account_number<<"\n";
     }
- 
+    void display(){
+        std::cout<<"\n"<<name<<"\n";
+        std::cout<<balance<<"\n";
+        std::cout<<account_number<<"\n";
+    }
+//Copy constructor
+    Customer(Customer &B){
+        name= B.name;
+        account_number  = B.account_number;
+        balance=B.balance;
+    }
 
 
 };
@@ -27,4 +37,11 @@ class Customer{
 int main(){
     Customer AI ("kaushiki",875667,892387388);
     Customer A2;
+    Customer A3(AI);
+    A3.display();
+    Customer A4(AI);
+    A4.display();
+    Customer A5;
+    A5=A3;
+    A5.display();
 }
